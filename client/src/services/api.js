5ab8e6fd0e0dd2fb2fbbb0e322152d2d20ad2c62
Mayank Tyagi <tyagimayank0501@gmail.com>
2026-06-13 17:28:31 +0530

    added production url]

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const api = axios.create({
-   baseURL: 'http://localhost:5000/api',
-  // baseURL:'https://wedding-crm-backend.vercel.app/api',
-  // headers: {
-  //   'Content-Type': 'application/json',
-  // },
+  //  baseURL: 'http://localhost:5000/api',
+  baseURL:'https://wedding-crm-backend.vercel.app/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
 
 export default api
